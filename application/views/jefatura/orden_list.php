e8aaa199ee7b62cd8225865fa2c51ff328491ddc
ccmansilla <carloscmansilla@gmail.com>
2022-04-07 13:37:43 -0300

    add modal

--- application/views/jefatura/orden_list.php
+++ application/views/jefatura/orden_list.php
@@ -16,12 +16,15 @@
 	</thead>
 	<tbody>
 	<?php foreach ($orders as $order): ?>
+		<?php $id = $order['id']; ?>
 	    <tr>
 			<td><?php echo $order['type']; ?></td>
 	        <td><?php echo $order['number']."/".$order['year']; ?></td>
 			<td><?php echo $order['about']; ?></td>
 	        <td><a href='<?php echo site_url('uploads/'.$order['file']); ?>' target="_blank" >abrir</a></td>
-			<td><a href='<?php echo site_url('jefatura/order_views/'.$order['id']); ?>' target="_blank">listar</a></td>
+			<td><a data-toggle="modal" onclick="modal(<?php echo $id; ?>)" href="#">   
+				<!-- href='<?php echo site_url('jefatura/order_views/'.$order['id']); ?>' target="_blank">
+	--> listar</a></td>
 	        <td><a class="btn btn-primary" href="<?= base_url('jefatura/order_edit/') . $order['id'] ?>" role="button">editar</a> <a class="btn btn-danger" href="<?=base_url('jefatura/order_delete/') . $order['id'] ?>" role="button">borrar</a></td>
 	    </tr>
 	<?php endforeach; ?>
@@ -31,5 +34,35 @@
 <div class="pagination">
 	<?= $pagination ?>
 </div>
+  <!-- The Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Lista de Usuarios</h4>
+          <button type="button" class="close" data-dismiss="modal">×</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body" id="modal__content">
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
 </div>
 </div>
+<script>
+	function modal(id){
+		$("#modal__content").text(id);
+		$("#myModal").modal()
+	}
+</script>
