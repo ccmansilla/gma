86d5a2f0300072639bfc3abb3786d8316e6dadc2
ccmansilla <carloscmansilla@gmail.com>
2022-04-28 07:39:44 -0300

    cambio vista ordenes para dependencias

--- application/views/dependencia/orden_list.php
+++ application/views/dependencia/orden_list.php
@@ -1,54 +1,45 @@
 
-	<div class="row" style="height: 700px;">
-	  <div class="col-sm-2">
-		<div class="table-orden">          
-		<table class="table table-bordered table-striped">
-			<thead>
-				<tr>
-				<th>Número</th>
-				</tr>
-			</thead>
-			<tbody>	
-				<?php foreach ($orders as $order): ?>
-					<tr>
-						<td>
-							<label for="visto">
-								<a href="#" onclick="cambiar_vista('<?php echo site_url('uploads/'.$order['file']);?>')" title="<?php echo $order['about'];?>">
-									<?php echo $order['type']." ".$order['number']."/".$order['year']; ?>
-								</a>
-							</label>
-							<?php echo form_open("dependencia/view/$type/$from");?>	
-								<?php
-									$id = $order['id'];
-									if ($order['id_order'] != NULL) {
-										echo "<input type='checkbox' name='id_order' checked='cheked' disabled>";
-									} else {
-										echo "<input type='checkbox' name='id_order' onchange='submit();' value='$id'>";
-									}
-								?>
-							</form>
-						</td>
-					</tr>
-				<?php endforeach ?>
-			</tbody>
-		</table>
-		</div>
-	  </div>
-		<div class="col-sm-10" id="view_container"> 
-			<embed id="vista" frameborder="0" width="100%" height="100%">
-		</div>
-	</div>
+	<div class="table-orden">          
+	<table class="table">
+	<thead class="thead-dark">
+		<tr>
+		<th style="width: 10%;">Tipo</th>
+		<th style="width: 10%;">Número</th>
+		<th style="width: 60%;">Tema</th>
+		<th style="width: 10%;">Archivo</th>
+		<th style="width: 10%;">Vistos</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($orders as $order): ?>
+		<?php 
+			$id = $order['id']; 
+			$numero = $order['number']."/".$order['year'];
+		?>
+	    <tr>
+			<td><?= $order['type']; ?></td>
+	        <td><?= $numero; ?></td>
+			<td><?= $order['about']; ?></td>
+	        <td><a href='<?php echo site_url('uploads/'.$order['file']); ?>' target="_blank" >abrir</a></td>
+			<td>
+			<?php echo form_open("dependencia/view/$type/$from");?>	
+				<?php
+					$id = $order['id'];
+					if ($order['id_order'] != NULL) {
+						echo "<input type='checkbox' name='id_order' checked='cheked' disabled>";
+					} else {
+						echo "<input type='checkbox' name='id_order' onchange='submit();' value='$id'>";
+					}
+				?>
+			</form>
+			</td>
+	    </tr>
+	<?php endforeach; ?>
+	</tbody>
+	</table>
 	<div class="row" id="pages">
 		<ul class="pagination">
 			<?= $pagination ?>
 		</ul>
 	</div>	
 	</div>
-
-	<script>
-		function cambiar_vista(source){
-			var parent = $("#vista").parent();
-			$("#vista").remove();
-			parent.append("<embed id='vista' frameborder='0' width='100%' height='100%' src=" + source + " />");
-		}
-	</script>
